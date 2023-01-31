2b365a185d007e304ba7afbd06f63cef2e7c0663
Julesswiss <jerhivwor@gmail.com>
2023-01-31 14:53:35 +0100

    clean

--- 0x06-regular_expressions/0-simply_match_school.rb
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env ruby
-#regular expression must match School
-puts ARGV[0].scan(/School/).join
-
--- 0x06-regular_expressions/6-phone_number.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-#regular expression must match a 10 digit phone number
-puts ARGV[0].scan(/^\d{10,10}$/).join
