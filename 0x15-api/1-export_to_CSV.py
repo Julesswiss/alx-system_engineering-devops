9bfee98b4b594fa289f0863e973d8f7394826f13
Julesswiss <jerhivwor@gmail.com>
2023-04-24 19:35:41 +0100

    API's not abc

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-""" this is to exports to-do list information for a given employee ID to CSV format."""
-import csv
+"""this gets data from json placeholder"""
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+if __name__ == '__main__':
+    """REST API manipulations"""
+    if len(sys.argv) > 1 and isinstance(eval(sys.argv[1]), int):
+        pass
+    else:
+        sys.exit(0)
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    BASE_API = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    user_response_url = BASE_API + "users/{}".format(employee_id)
+    todo_response_url = BASE_API + "users/{}/todos".format(employee_id)
+
+    user_response = requests.get(user_response_url).json()
+    todo_response = requests.get(todo_response_url).json()
+
+    employee_name = user_response.get('name')
+    username = user_response.get('username')
+
+    with open("{}.csv".format(employee_id), 'w') as f:
+        for todo in todo_response:
+            task_status = todo.get("completed")
+            task_title = todo.get("title")
+            to_write = '"{}","{}","{}","{}"\n'.format(
+                employee_id, username, task_status, task_title)
+            f.write(to_write)
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,46 @@
 #!/usr/bin/python3
-"""This exports to-do list information for a given employee ID to JSON format."""
-import json
+"""getting data from jsonplaceholder"""
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+if __name__ == '__main__':
+    """REST API manipulations here"""
+    if len(sys.argv) > 1 and isinstance(eval(sys.argv[1]), int):
+        pass
+    else:
+        sys.exit(0)
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+    BASE_API = "https://jsonplaceholder.typicode.com/"
+    employee_id = sys.argv[1]
+    user_response_url = BASE_API + "users/{}".format(employee_id)
+    todo_response_url = BASE_API + "users/{}/todos".format(employee_id)
+
+    user_response = requests.get(user_response_url).json()
+    todo_response = requests.get(todo_response_url).json()
+
+    employee_name = user_response.get('name')
+    username = user_response.get('username')
+
+    with open("{}.json".format(employee_id), 'w') as f:
+        f.write('{')
+        f.write('"{}"'.format(employee_id))
+        f.write(':[')
+        for i, todo in enumerate(todo_response):
+            task_status = todo.get("completed")
+            task_title = todo.get("title")
+            if i == len(todo_response) - 1:
+                f.write(
+                    '{{"task": "{}", "completed": {}, "username": "{}"}}'.
+                    format(
+                        task_title,
+                        'true' if task_status else 'false',
+                        username))
+            else:
+                f.write(
+                    '{{"task": "{}", "completed": {}, "username": "{}"}}, '.
+                    format(
+                        task_title,
+                        'true' if task_status else 'false',
+                        username))
+
+        f.write(']}')
